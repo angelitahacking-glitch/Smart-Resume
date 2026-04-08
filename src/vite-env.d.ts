5af77f4c968fd3c5edda1567d4eec3f4093a7a59
Angelita <angelitahacking@gmail.com>
2026-04-08 22:56:37 +0800

    修复合并冲突并优化代码

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_*: string;
+  [key: string]: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
-=======
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_*: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
->>>>>>> f5896852d5d3cc2eea49229113552b8187814cbb
 }
